47c2e3b2263d87bf7223aca27f4cdc2eccf114d9
Jonathan R. M. <jonatanrm92@gmail.com>
2022-05-08 18:59:28 -0300

    IndexController, controller,mapping

--- TP5_Grupo23/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ TP5_Grupo23/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -1,10 +1,49 @@
 package ar.edu.unju.fi.model;
 
 public class Alumno {
-		int dni;
-		int telefono;
-		String nombre;
-		String apellido;
-		String email;
-		
+	private String nombres;
+	private String apellido;
+	private String eMail;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Alumno(String nombres, String apellido, String eMail) {
+		super();
+		this.nombres = nombres;
+		this.apellido = apellido;
+		this.eMail = eMail;
+	}
+
+
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	
+	
+
 }
