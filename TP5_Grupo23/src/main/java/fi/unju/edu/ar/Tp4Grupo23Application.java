16dffdbec75ffec31ac45ebe5f066537d13999b1
CASA <CASA@DESKTOP-09O0OPO>
2022-05-08 14:13:01 -0300

    tp4

---
+++ TP5_Grupo23/src/main/java/fi/unju/edu/ar/Tp4Grupo23Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo23Application.class, args);
+	}
+
+}
